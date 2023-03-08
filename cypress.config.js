6bf6f25a28bfcb5e96ac3be1b1d79c1392d47471
Kevin <kevinagustinez@gmail.com>
2023-03-07 23:17:26 -0300

    Agregando Libreria ReportHTML

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '8repu8',
   chromeWebSecurity:false,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: { 
     experimentalStudio: true,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
     // implement node event listeners here
     },
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,6 +18,7 @@ import '@applitools/eyes-cypress/commands'
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
